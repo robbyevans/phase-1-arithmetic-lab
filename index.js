58f1b731c3c7bc4a46615086a2bb93e3194a0aee
Evans Rutto <robbyevans001@gmail.com>
2022-06-06 22:44:24 +0300

    commit tree 33095f0f0fe4895fa35e0e5ff21da320d26dab12

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1=31;
+let num2=2;
+let num3=24;
+let num4=5;
 
-// Write your code here
+let multiply=(num1*num2);
+
+
+let random=(Math.floor(Math.random() + 1));
+
+let mod=(num3%num4);
+
+let max=(Math.max(1,2,3,5,15,20));
+
+
+
+
